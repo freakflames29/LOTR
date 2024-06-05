252a0aa36108e5e7095b0b55d8fe6cb0e394dd5e
Sourav Das <iamsouravdaswade420@gmail.com>
2024-06-05 16:46:47 +0530

    Initial Commit

---
+++ LOTR/LOTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRApp.swift
+//  LOTR
+//
+//  Created by sourav das on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
